ce9617f9dc463238d4b41c3489ca529ea8ecdd64
jnparry <jordanwahparry@gmail.com>
2020-07-04 14:13:12 -0500

    probability test

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ function calculateOnePlusOne() {
 }
 
 function calculateItemDungeon() {
-	var text = document.getElementById("ItemDugeon");
-	text.value = "hello";
+	var text = "ItemDugeon";
+	var probabilityobject = new Probability({
+		p: '10',
+		f: setText(text, "Weapon")
+	}, {
+		p: '90',
+		f: setText(text, "Armor")
+	})
 }
 
 function calculateItemContainer() {
@@ -17,3 +23,70 @@ function calculateItemGehennom() {
 	var text = document.getElementById("ItemGehennom");
 	text.value = "hello";
 }
+
+function setText(elementId, textValue) {
+	document.getElementById(elementId).value = textValue;
+}
+
+
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.Probability = factory();
+    }
+})(this, function () {
+
+    var toString = Object.prototype.toString,
+        slice = Array.prototype.slice;
+
+    function Probability() {
+
+        var i = 0,
+            l = 0,
+            probas = [],
+            functions = [],
+            sum = 0,
+            args = toString.call(arguments[0]) === '[object Array]' ? arguments[0] : slice.call(arguments);
+
+        args.push({
+            p: 0,
+            f: function () {}
+        });
+
+        for (i = 0, l = args.length; i < l; i++) {
+            var p = Math.abs(parseFloat(args[i].p)),
+                f = args[i].f;
+
+            if (isNaN(p) || typeof f !== 'function') {
+                throw new TypeError('Probability.js: Invalid probability object in argument ' + i + '.');
+            }
+
+            if (/%/.test(args[i].p)) {
+                p = p / 100.0;
+            }
+
+            sum += p;
+
+            if (sum > 1.0) {
+                throw new TypeError('Probability.js: Probability exceeds "1.0" (=100%) in argument ' + i + ': p="' + p + '" (=' +  p * 100 + '%), sum="' + sum + '" (=' +  sum * 100 + '%).');
+            }
+
+            probas[i] = sum;
+            functions[i] = f;
+        }
+
+        return function probabilitilized() {
+            var random = Math.random();
+            for (i = 0, l = probas.length - 1; i < l && random >= probas[i]; i++) {
+                /* intentionally left empty */
+            }
+            return functions[i].apply(this, arguments);
+        };
+    }
+
+    return Probability;
+
+});
