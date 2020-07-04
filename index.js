57eb8ce602d2e7226fb4cbb49f5bb5dc180827f7
jnparry <jordanwahparry@gmail.com>
2020-07-04 11:58:26 -0500

    add button functionality

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function calculateOnePlusOne() {
 	var param1 = 1;
 	var param2 = 1;
 	var calculatedValue = document.getElementById("result");
-	calculatedValue.innerHTML = param1 + param2;
+	calculatedValue.innerHTML = "yo";
 }
